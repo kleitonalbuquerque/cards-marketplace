fe7823cb7584b7d26398d36c6c09e1464e021a86
Kleiton Albuquerque <kleiton2102@gmail.com>
2025-07-24 20:14:14 -0300

    [refact]Ajuste no vite.config para passar no deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-// Corrige o dirname para ambientes ESM
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
